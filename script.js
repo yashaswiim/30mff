a22f6a11f972b1c1e7485ecaa51b439189a6fdd6
Yashaswi Malla <mallayashaswi@gmail.com>
2022-09-19 22:56:46 +0400

    updates to the layout
    photo carousel, responsive layout, homepage, fixed formats

--- script.js
+++ script.js
@@ -1,12 +1,56 @@
-window.onscroll = function() {myFunction()};
+var index = {
+    one: 0,
+    two: 0,
+    three: 0,
+    four: 0,
+    five: 0,
+    six: 0
+}
 
-var intro = document.querySelector("#intro")
+var images_all = document.querySelectorAll(".counter img")
+var images_one = document.querySelectorAll(".imgs img:first-of-type")
+var prev_all = document.getElementsByClassName("prev")
+var next_all = document.getElementsByClassName("next")
 
-function myFunction() {
-  if (window.pageYOffset >= 100) {
-    intro.style.display = "none"
-  }
-  else{
-    intro.style.display = ""
-  }
+const nextImage = (p)=>{
+    showImage(p, index[p]+=1)
+}
+
+const prevImage = (p)=>{
+    showImage(p, index[p]-=1)
+}
+
+const showImage = (p, ind)=>{
+    let q = "."+p+" img"
+    let imgs = document.querySelectorAll(q)
+    if (ind<imgs.length && ind>=0){
+        for (let j = 0; j<imgs.length;j++){
+            imgs[j].style.display="none"
+        }
+        imgs[ind].style.display=""
+    }
+    else{
+        index[p]-=1
+    }
+}
+
+for (i = 0; i < images_all.length; i++) {
+    images_all[i].style.display = "none";  
+}
+
+for (i=0; i < Object.keys(index).length;i++){
+    showImage(Object.keys(index)[i], 0)
+}
+
+for(var i=0;i<prev_all.length;i++){
+    prev_all[i].addEventListener("click", (e)=>{
+        p = e.path[1].classList[1]
+        prevImage(p)
+    })
+}
+for(var i=0;i<next_all.length;i++){
+    next_all[i].addEventListener("click", (e)=>{
+        p = e.path[1].classList[1]
+        nextImage(p)
+    })
 }
